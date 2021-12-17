eb3365c386a49a369e4946558a3ca9e0faf116ec
BrandonSmith710 <75755695+BrandonSmith710@users.noreply.github.com>
2021-12-16 21:19:45 -0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import Day, DB
-from api import make_query
+from .models import Day, DB
+from .api import make_query
 
 
 def create_app():
